cf128e7cb696e44cb7e02ab000baea7f4b43ae4b
M.Haris Khan <hkhan@cedrus.com>
2023-07-27 18:02:01 +0500

    Feat;StrangePair

---
+++ StrangePair/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class program
+{
+   
+    public static bool StrangePair(string str1, string str2)
+    {
+
+        if (string.IsNullOrEmpty(str1) && string.IsNullOrEmpty(str2))
+        {
+            return true;
+        }
+
+        if (str1.StartsWith(str2.Substring(str2.Length - 1)) && str2.StartsWith(str1.Substring(str1.Length - 1)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void Main()
+    {
+        String text1;
+        String text2;
+        Console.WriteLine("Enter a text:");
+        text1 = Console.ReadLine();
+        text2 = Console.ReadLine();
+        bool value= StrangePair(text1,text2);
+
+        Console.WriteLine(value);
+        Console.ReadLine();
+    }
+       
+       
+
+
+
+
+
+
+
+    
+}
